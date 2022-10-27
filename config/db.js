eab94b1e6f041b2927d8a407c616c159de2472e9
korospace <bagaselkoro@gmail.com>
2022-10-27 18:46:00 +0700

    fetch from master

--- config/db.js
+++ config/db.js
@@ -1,14 +1,17 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    host:'ec2-54-147-36-107.compute-1.amazonaws.com',
-    port:'5432',
-    user:'xegiidcmedfazi',
-    database:'d2jmb5m8fsud5f',
-    password:'c033081de4cc9fe453dbedacc48516c965411d7651f011df11ae37904a1d865a',
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    host:'containers-us-west-28.railway.app',
+    port:'6650',
+    user:'postgres',
+    database:'railway',
+    password:'lEqgw3VdFiGSEtK15iBt',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 })
 
 module.exports = pool;
